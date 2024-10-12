0934df362c8ce32986cc254399af966c9b976eb9
kolex <kovalenkolex63@gmail.com>
2024-10-12 20:12:33 +0300

    first

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.12
+
+RUN mkdir -p /opt/weather_bot/bot
+RUN mkdir -p /opt/weather_bot/sql
+WORKDIR /home/ansible/weather_bot/bot
+
+COPY . /opt/weather_bot/bot
+RUN pip install --no-cache-dir -r requirements.txt
+
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,171 @@
+import types
+import telebot
+import webbrowser
+from telebot import types
+import sqlite3
+import openmeteo_requests
+from openmeteo_sdk.Variable import Variable
+
+
+bot = telebot.TeleBot('7296472862:AAHw8kxmi_m7eVGa6wQUBBhKe3Y9A0jrkNs')
+place_name1 = ''
+la = ''
+lo = ''
+
+
+def weather(lati, longi):
+    om = openmeteo_requests.Client()
+    params = {
+        "latitude": {lati},
+        "longitude": {longi},
+        "current": ["temperature_2m"]
+    }
+    responses = om.weather_api("https://api.open-meteo.com/v1/forecast", params=params)
+    response = responses[0]
+    current = response.Current()
+    current_variables = list(map(lambda i: current.Variables(i), range(0, current.VariablesLength())))
+    current_temperature_2m = next(
+        filter(lambda x: x.Variable() == Variable.temperature and x.Altitude() == 2, current_variables))
+    return current_temperature_2m.Value()
+
+def latitude(message):
+    global la
+    la = message.text.strip()
+    bot.send_message(message.chat.id, 'Введите долготу, используя точку как разделитель\n'
+                                      'Например 12.123 (значение от -180 до 180):')
+    bot.register_next_step_handler(message, longitude)
+
+def longitude(message):
+    global lo
+    global la
+    lo = message.text.strip()
+    bot.send_message(message.chat.id, f'Там сейчас {round(weather(la, lo))} ℃')
+    # Кнопки с вопросом
+    mark = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Да', callback_data='yes')
+    btn2 = types.InlineKeyboardButton('Нет', callback_data='no')
+    mark.row(btn1, btn2)
+    bot.send_message(message.chat.id, 'Сохранить это место?', reply_markup=mark)
+
+# def question(message):
+    # global lo
+    # global la
+    # bot.send_message(message.chat.id, f'Там сейчас {round(weather(la, lo))} ℃')
+
+
+def save_place(message):
+    place_name1 = message.text.strip()
+    conn = sqlite3.connect('basa.sql')
+    cur = conn.cursor()
+    update_statement = 'UPDATE users SET latitude=?, longitude=?, place_name=? WHERE tg_id = ?'
+    cur.execute(update_statement, (la, lo, place_name1, message.from_user.id))
+    conn.commit()
+    cur.close()
+    conn.close()
+    bot.send_message(message.chat.id, f'Сохранено место "{place_name1}" с координатами: \n'
+                                      f'Широта {la} Долгота {lo}')
+
+
+@bot.message_handler(commands=['start', 'main'])
+def start(message):
+    # global numero
+    # Создание БД
+    conn = sqlite3.connect('basa.sql')
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS users (tg_id int, first_name varchar(50), last_name varchar(50),'
+                'latitude float, longitude float, place_name varchar(50), payment int DEFAULT 0)')
+    cur.execute('SELECT tg_id FROM users')
+    tgid = cur.fetchall()
+    # Добавление инфы про пользователя в БД
+    # print(f'tip mfi {type(message.from_user.id)}')
+    # print(f'type tgid {type(tgid)}')
+    # print(f'tgid = {tgid[0]}')
+    ids = str(tgid)[1:-1].replace('(', '').replace(')',
+                                                   '').replace(',', '').split(' ')
+    # print(f'vot ono{type(ids[0])}')
+    if ids == ['']:
+        int_ids = [None]
+    else:
+        int_ids = [int(item) for item in ids]
+    # print(int_ids)
+    # print(type(int_ids))
+    if message.from_user.id in int_ids:
+        pass
+    else:
+        cur.execute('INSERT INTO users (tg_id, first_name, last_name) VALUES (?, ?, ?)',
+                (message.from_user.id, message.from_user.first_name, message.from_user.last_name))
+    cur.execute('SELECT * FROM users')
+    users = cur.fetchall()
+    conn.commit()
+    cur.close()
+    conn.close()
+    # print(users)
+    
+    # Вывод стартовых кнопок
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Санкт-Петербург', callback_data='spb')
+    btn2 = types.InlineKeyboardButton('Москва', callback_data='msk')
+    markup.row(btn1, btn2)
+    btn3 = types.InlineKeyboardButton('Тольятти', callback_data='tlt')
+    btn4 = types.InlineKeyboardButton('Другое', callback_data='else')
+    markup.row(btn3, btn4)
+    bot.send_message(message.chat.id, 'Привет! Чтобы получить прогноз погоды, выберите город:', reply_markup=markup)
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, '<b>Помощь</b> с ботом: \n'
+                                      'Знаю команды <em>/help,</em> Привет', parse_mode='html')
+
+
+#Действия при нажатии на кнопки
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    global la
+    global lo
+    # global place_name1
+    if callback.data == 'msk':
+        la = 55.7522
+        lo = 37.6156
+        bot.send_message(callback.message.chat.id, f'В Москве сейчас {round(weather(la, lo))} ℃')
+    elif callback.data == 'tlt':
+        la = 53.5303
+        lo = 49.3461
+        bot.send_message(callback.message.chat.id, f'В Тольятти сейчас {round(weather(la,lo))} ℃')
+    elif callback.data == 'spb':
+        la = 59.9386
+        lo = 30.3141
+        bot.send_message(callback.message.chat.id, f'В Санкт-Петербурге сейчас {round(weather(la, lo))} ℃')
+    elif callback.data == 'else':
+        bot.send_message(callback.message.chat.id, 'Укажите координаты места.\n'
+                                                    'Сначала введите широту, используя точку как разделитель \n'
+                                                   'Например 12.123 (значение от -90 до 90):')
+        # bot.send_message(callback.message.chat.id, 'Введите широту')
+        bot.register_next_step_handler(callback.message, latitude)
+        # la = message.text.strip()
+        # la = float(input())
+        # bot.send_message(callback.message.chat.id, 'Введите долготу')
+        # lo = float(input())
+        # la = message.text.strip()
+    elif callback.data == 'yes':
+        bot.send_message(callback.message.chat.id, 'Введите название для этого места:')
+        # place_name1 = callback.message.text.strip()
+        bot.register_next_step_handler(callback.message, save_place)
+
+        ## place_name1 = callback.message.text.strip()
+
+    elif callback.data == 'no':
+        bot.send_message(callback.message.chat.id, 'Окей, не сохраняем')
+
+
+
+# В случае рукописного текста
+@bot.message_handler()
+def greet(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}!')
+    else:
+        bot.send_message(message.chat.id, 'Я не знаю такую команду, попробуй другую')
+
+
+bot.polling(non_stop=True)
